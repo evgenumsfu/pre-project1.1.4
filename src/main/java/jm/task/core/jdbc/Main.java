9525c40d463a7238554f444614e192991e6d751a
evgenumsfu <evgenumsfu@mail.ru>
2023-12-17 00:01:06 +0700

    версия

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,21 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static UserService userService = new UserServiceImpl();
+
+    public static void main(String[] args) {
+
+        userService.createUsersTable();
+        userService.saveUser("Igor", "Gromov", (byte) 22);
+        userService.saveUser("Oleg", "Ivanov", (byte) 45);
+        userService.saveUser("Roma", "Durov", (byte) 7);
+        userService.saveUser("Dima", "Petrov", (byte) 15);
+
+        userService.getAllUsers().forEach(System.out::println);       // получение всех user и печать
+        userService.cleanUsersTable();                                // очистка таблицы
+        userService.dropUsersTable();                                 // удаление табоицы
+    }
+}
